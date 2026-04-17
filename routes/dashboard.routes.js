56d4471a7863f906ebad85e7931f1c387d0ca734
KingSadid <SadiSex07@gmail.com>
2026-04-16 19:11:11 -0500

    We restarted the project

--- routes/dashboard.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboard.controller');
-const { verifyToken } = require('../middlewares/auth.middleware');
-
-router.get('/kpis', verifyToken, dashboardController.getDashboardKPIs);
-
-module.exports = router;
--- routes/product.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/product.controller');
-const { verifyToken } = require('../middlewares/auth.middleware');
-
-router.get('/', verifyToken, productController.getAll);
-router.get('/:id', verifyToken, productController.getOne);
-
-module.exports = router;
