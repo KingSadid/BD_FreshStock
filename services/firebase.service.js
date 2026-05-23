106bc14492c1c1fae4e3a7d6bb3d3271f95f3db7
KingSadid <SadiSex07@gmail.com>
2026-05-23 12:18:41 -0500

    Final corrections + new JSON key

--- services/firebase.service.js
+++ services/firebase.service.js
@@ -3,6 +3,12 @@ const path = require('path');
 
 const serviceAccount = require(path.join(__dirname, '..', 'env', 'freshstock_key.json'));
 
+if (serviceAccount.private_key && !serviceAccount.private_key.includes('BEGIN PRIVATE KEY')) {
+  serviceAccount.private_key = `-----BEGIN PRIVATE KEY-----\n${serviceAccount.private_key.replace(/\\n/g, '\n')}\n-----END PRIVATE KEY-----\n`;
+} else if (serviceAccount.private_key) {
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+}
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
