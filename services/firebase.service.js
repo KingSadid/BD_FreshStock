b9467b52441cc530333391af36274edebd13388d
KingSadid <SadiSex07@gmail.com>
2026-05-22 21:41:09 -0500

    restoring

--- services/firebase.service.js
+++ services/firebase.service.js
@@ -1,24 +1,7 @@
 const admin = require('firebase-admin');
 const path = require('path');
-const fs = require('fs');
 
-const envPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
-let resolvedPath = path.join(__dirname, '..', 'env', 'freshstock_key.json');
-
-if (envPath) {
-  const potentialPath = path.resolve(envPath);
-  if (fs.existsSync(potentialPath)) {
-    resolvedPath = potentialPath;
-  } else {
-    const rootPotentialPath = path.join(__dirname, '..', envPath);
-    if (fs.existsSync(rootPotentialPath)) {
-      resolvedPath = rootPotentialPath;
-    }
-  }
-}
-
-console.log(`[Firebase] Cargando credenciales desde: ${resolvedPath}`);
-const serviceAccount = require(resolvedPath);
+const serviceAccount = require(path.join(__dirname, '..', 'env', 'freshstock_key.json'));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
